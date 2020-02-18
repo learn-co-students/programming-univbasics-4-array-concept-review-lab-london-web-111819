1d514e2d4f0b458a5c4fddc7f19f8e4f93444d90
Ali G Junejo <aligohar_junejo@live.com>
2020-02-18 17:06:16 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,14 +1,15 @@
-  
+
+
 def find_element_index(array, value_to_find)
-  array.find_index(value_to_find)
+  array.index(value_to_find)
+
 end
 
+
 def find_max_value(array)
-  array2 = array.sort.reverse
-  return array2[0]
+  array.max
 end
 
 def find_min_value(array)
-  new_array = array.sort
-  return new_array[0]
+  array.min
 end
